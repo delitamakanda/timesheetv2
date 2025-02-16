e04186702be5d416605981b0a4fda507c2c18224
Délita Makanda <delita.makanda@gmail.com>
2025-02-16 23:54:54 +0100

    add factory

---
+++ core/factory/factory.py
@@ -0,0 +1,19 @@
+from functools import partial
+from fastapi import Depends
+
+from app.controllers import AuthController, UserController
+from app.models import User
+from app.repositories import UserRepository
+from core.database import get_session
+
+class Factory:
+    """
+    Factory for creating controllers and repositories.
+    """
+    user_repo = partial(UserRepository, User)
+    
+    def get_user_controller(self, db_session = Depends(get_session)):
+        return UserController(self.user_repo(db_session))
+    
+    def get_auth_controller(self, db_session = Depends(get_session)):
+        return AuthController(self.user_repo(db_session))
