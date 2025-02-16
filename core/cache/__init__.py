ba25624711753d558161df743d6ab8ecbb0f12cb
Délita Makanda <delita.makanda@gmail.com>
2025-02-16 22:09:46 +0100

    add cache modules

---
+++ core/cache/__init__.py
@@ -0,0 +1,11 @@
+from .cache_tag import CacheTag
+from .custom_key_maker import CustomKeyMaker
+from .redis_backend import RedisBackend
+from .cache_manager import Cache
+
+__all__ = [
+    "CacheTag",
+    "CustomKeyMaker",
+    "RedisBackend",
+    "Cache",
+]
