1c94e812be91f4fcda77b57fb878a299db3c598c
Délita Makanda <delita.makanda@gmail.com>
2025-03-03 01:21:51 +0100

    fix db session

--- core/database/session.py
+++ core/database/session.py
@@ -1,37 +1,47 @@
 from contextvars import ContextVar, Token
-from typing import Union, Optional
+from typing import Union
 
-from sqlalchemy.ext.asyncio import AsyncSession, async_scoped_session, create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_scoped_session,
+    create_async_engine,
+)
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 from sqlalchemy.sql.expression import Delete, Insert, Update
 
 from core.config import config
 
 session_context: ContextVar[str] = ContextVar("session_context")
 
+
 def get_session_context() -> str:
     return session_context.get()
 
-def set_session_context(session_id: str) -> Optional[Token]:
+
+def set_session_context(session_id: str) -> Token:
     return session_context.set(session_id)
-    
+
+
 def reset_session_context(context: Token) -> None:
     session_context.reset(context)
-    
+
+
 engines = {
     "writer": create_async_engine(config.DATABASE_URL, pool_recycle=3600),
     "reader": create_async_engine(config.DATABASE_URL, pool_recycle=3600),
 }
 
-class RoutingSession(AsyncSession):
+
+class RoutingSession(Session):
     def get_bind(self, mapper=None, clause=None, **kwargs):
-        if self.in_transaction() or isinstance(clause, (Update, Delete, Insert)):
-            return engines["writer"]
-        return engines["reader"]
+        if self._flushing or isinstance(clause, (Update, Delete, Insert)):
+            return engines["writer"].sync_engine
+        return engines["reader"].sync_engine
 
 
 async_session_factory = sessionmaker(
     class_=AsyncSession,
+    sync_session_class=RoutingSession,
     expire_on_commit=False,
 )
 
@@ -48,8 +58,10 @@ async def get_session():
 
     :return: The database session.
     """
-    async with session() as s:
-        yield s
+    try:
+        yield session
+    finally:
+        await session.close()
 
 
 Base = declarative_base()
