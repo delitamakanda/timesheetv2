65f4fb9de6a9ad3fc4892299a035c04bd21e5bfc
Délita Makanda <delita.makanda@gmail.com>
2025-03-03 01:23:09 +0100

    add deps current user

--- core/dependencies/current_user.py
+++ core/dependencies/current_user.py
@@ -1,1 +1,9 @@
 from fastapi import Depends, Request
+from app.controllers.user import UserController
+from core.factory import Factory
+
+async def get_current_user(
+        request: Request,
+        controller: UserController = Depends(Factory().get_user_controller),
+):
+    return await controller.get_by_id(request.user.id)
