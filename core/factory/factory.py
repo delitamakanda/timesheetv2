c6537fc49d90244c489a7312f2ed84447254ad23
Délita Makanda <delita.makanda@gmail.com>
2025-03-07 23:01:11 +0100

    refactor factory

--- core/factory/factory.py
+++ core/factory/factory.py
@@ -13,7 +13,7 @@ class Factory:
     user_repo = partial(UserRepository, User)
     
     def get_user_controller(self, db_session = Depends(get_session)):
-        return UserController(self.user_repo(db_session))
+        return UserController(user_repo=self.user_repo(db_session=db_session))
     
     def get_auth_controller(self, db_session = Depends(get_session)):
-        return AuthController(self.user_repo(db_session))
+        return AuthController(user_repo=self.user_repo(db_session=db_session))
